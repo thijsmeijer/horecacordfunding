f60a25d11e12f37074ebdbdb312c58363c57fcb5
thijsmeijer <t.meijer@ifresh.nl>
2022-11-11 11:21:39 +0100

    HCF-45  Als gebruiker wil ik mijn gegevens kunnen inzien.

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -10,6 +9,8 @@ class ProfileController extends Controller
 {
     public function index(): Response
     {
-        return Inertia::render('Profile/Information');
+        return Inertia::render('Profile/Information', [
+            'user' => auth()->user(),
+        ]);
     }
 }
