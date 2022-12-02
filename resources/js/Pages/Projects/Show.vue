5238573a675161239affd955bc61b2720b253d58
Kasper Ligthart <k.ligthart@ifresh.nl>
2022-12-02 11:25:31 +0100

    fix merge conflicts

--- resources/js/Pages/Projects/Show.vue
+++ resources/js/Pages/Projects/Show.vue
@@ -3,6 +3,21 @@
         <img src="https://picsum.photos/800/200" class="object-cover h-48 w-full" alt="">
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="rounded-md bg-green-50 p-4" v-if="$page.props.flash.success">
+                    <div class="flex">
+                        <div class="flex-shrink-0">
+                            <svg class="h-5 w-5 text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                                 fill="currentColor" aria-hidden="true">
+                                <path fill-rule="evenodd"
+                                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                                      clip-rule="evenodd"/>
+                            </svg>
+                        </div>
+                        <div class="ml-3">
+                            <p class="text-sm font-medium text-green-800">{{ $page.props.flash.success }}</p>
+                        </div>
+                    </div>
+                </div>
                 <div>
                     <a :href="$page.props.previousUrl">
                         <dl class="flex items-center space-x-2 text-blue-500">
