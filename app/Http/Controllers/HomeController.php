1389177f8247805fded1bb0b557dbaf7c5558bb5
Kasper Ligthart <k.ligthart@ifresh.nl>
2022-12-05 16:05:40 +0100

    HCF-73 Als gast wil ik de 4 meest recent aangemaakte projecten bovenin zien. && HCF-74 Als gast wil ik de overige projecten gesorteerd hebben op funding.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Resources\ProjectIndexResource;
 use App\Models\Project;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
 class HomeController extends Controller
@@ -17,8 +18,12 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
+        $newestProjects = Project::latest()->take(4)->get();
+        $highestProjects = Project::orderBy(DB::raw('(SELECT SUM(amount) /projects.amount * 100 FROM investments WHERE project_id = projects.id)'), 'desc')->take(8)->get();
+
         return Inertia::render('Home', [
-            'projects' => ProjectIndexResource::collection(Project::latest()->take(12)->get()),
+            'highestProjects' => ProjectIndexResource::collection($highestProjects),
+            'newestProjects' => ProjectIndexResource::collection($newestProjects),
         ]);
     }
 }
